038008f5fff68c7070a834936812546d92e6c2a5
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-02 17:05:42 -0400

    Finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,18 +1,67 @@
 package Step4;
 
+import java.io.FileNotFoundException;
+import java.io.OptionalDataException;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
-public class ReadFour
-{
+public class ReadFour {
     /**
-     * Yes, you can look at the other 
+     * Yes, you can look at the other
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
-            // Print out a running total of all the
-            // values in the input file.
+    public static void main(String[] args) throws FileNotFoundException {
+        // Print out a running total of all the
+        // values in the input file.
+
+        try {
+            Scanner fileIn = new Scanner(new File("Step4/input.txt"));
+            int sum = 0;
+
+            while (fileIn.hasNext())
+            {
+                // Reads the entire line as a string
+                String lineIn = fileIn.nextLine();
+                // Split the line into a String array
+                String[] input = lineIn.split(",");
+
+                for (int i = 0; i < input.length; i ++) {
+                    sum += Integer.parseInt(input[i]);
+                }
+                System.out.println("Running total = " + sum);
+            }
         }
+        catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
 }
+
+
+//        try {
+//        Scanner fileIn = new Scanner(new File());
+//        int sum = 0;
+//
+//        while (fileIn.hasNext()) {
+//            String lineIn = fileIn.nextLine().trim();
+//
+//            //Split the line
+//            String[] numbers = line.split("\\s+");
+//
+//
+//            for (int i) {
+//                try {
+//                    int number = Integer.parseInt(numberStr);
+//                    lineSum += number;
+//                } catch (NumberFormatException e) {
+//                    runningTotal += lineSum;
+//                }
+//
+//                System.out.println("Running total = " + runningTotal);
+//
+//                fileIn.close();
+//            }
+//        }
+//    }
+//}
